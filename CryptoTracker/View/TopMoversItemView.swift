f1e2ac617add24566036ea944e244dfc17b7ce15
mieng18 <mieng18@hotmail.com>
2023-07-19 16:01:29 -0700

    Darkmode Support - Color Set

--- CryptoTracker/View/TopMoversItemView.swift
+++ CryptoTracker/View/TopMoversItemView.swift
@@ -39,6 +39,7 @@ struct TopMoversItemView: View {
             
         }
         .frame(width: 120,height: 120)
+        .background(Color("itemBackgroundColor"))
         .overlay(
             RoundedRectangle(cornerRadius: 10)
                 .stroke(Color(.systemGray4),lineWidth:2)
